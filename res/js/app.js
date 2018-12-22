7569adbd55ef9ce50d39d00f4d854a42fbe2d0b7
Emil <emil93@gmail.com>
2018-12-23 00:00:03 +0100

    a search may now be made (must press enter still)

--- res/js/app.js
+++ res/js/app.js
@@ -1,19 +1,31 @@
 const ROOT = "https://api.github.com";
 
+/* IMPORTANT GRABBABLE ITEMS */
 const searchBox = document.getElementById("search");
 const resHeader = document.getElementById("res_header");
 const resBody1 = document.getElementById("res_body1");
 const resBody2 = document.getElementById("res_body2");
 const container = document.querySelector(".container");
-console.log(container);
+const searchResArea = document.getElementById("res-area");
+
 searchBox.onkeyup = (e) => {
-    if (!(searchBox.value.length > 4 && e.key === "Enter"))
+    if (!(searchBox.value.length > 2 && e.key === "Enter"))
         return;
     searchProfile(searchBox.value);
-    // displayUser(searchBox.value);
 }
 
-function createProfile(username, name, bio, avatar_url) {
+function purgeSearchAndDisplay(event) {
+    let wantedSource = event.path.find((ele) => {
+        return ele.className === "search-res";
+    });
+    let username = wantedSource.querySelector("p").textContent;
+    displayUser(username);
+    while (searchResArea.firstChild) {
+        searchResArea.removeChild(searchResArea.firstChild);
+    }
+}
+
+function displayProfile(username, name, bio, avatar_url) {
     // Create the container
     let profileContainer = document.createElement("div");
     profileContainer.className = "profile";
@@ -48,17 +60,55 @@ function createProfile(username, name, bio, avatar_url) {
     searchBox.value = "";
 }
 
+let isShowing = false;
+function displayResArea(shouldShow) {
+    isShowing = shouldShow;
+    searchResArea.style.display = shouldShow;
+}
+
+function displaySearchRes(username, avatar_url) {
+    let wrappingAnchor = document.createElement("a");
+    wrappingAnchor.href = "#";
+
+    let resContainer = document.createElement("div");
+    resContainer.className = "search-res";
+
+    let thumbnail = document.createElement("img");
+    thumbnail.src = avatar_url;
+
+    let displayName = document.createElement("p");
+    displayName.textContent = `${username}`;
+
+    resContainer.appendChild(thumbnail);
+    resContainer.appendChild(displayName);
+
+    wrappingAnchor.appendChild(resContainer);
+    searchResArea.appendChild(wrappingAnchor);
+
+    wrappingAnchor.onclick = purgeSearchAndDisplay;
+}
+
+const MAX_SEARCH_RES = 10;
 async function searchProfile(query) {
     let promise = await fetch(`${ROOT}/search/users?q=${query}`);
-    let res = await promise.json();
-    console.log(res);
+    if (promise.status !== 200) {
+        console.log("search fetch failed...");
+        return;
+    }
+    let data = await promise.json();
+    for (let i = 0; i < Math.min(data.total_count, MAX_SEARCH_RES); i++) {
+        displaySearchRes(data.items[i].login, data.items[i].avatar_url);
+    }
+    console.log(data);
 }
 
 async function displayUser(username) {
     let promise = await fetch(`${ROOT}/users/${username}`);
-    if (promise.status !== 200)
+    if (promise.status !== 200) {
+        console.log("user fetch failed...");
         return;
+    }
     let data = await promise.json();
     console.log(data);
-    createProfile(data.login, data.name, data.bio, data.avatar_url);
+    displayProfile(data.login, data.name, data.bio, data.avatar_url);
 }
